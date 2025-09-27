d4f7aa7ce866d50b3bba86fe52f1252a2e910b22
luckesi <ckluckesi@gmail.com>
2025-09-27 18:37:10 -0300

    versao com mais bugs galera

--- scripts/characters/carrie.gd
+++ scripts/characters/carrie.gd
@@ -1,13 +1,13 @@
 extends CharacterBody2D
 
 @onready var animation: AnimationPlayer = $AnimationPlayer
-
+@onready var texture: Sprite2D = $Texture
 @export_category("Player Stats")
 @export var SPEED: float = 150
 
 var current_dir: String = "none"
 
-func _physics_process(_delta: float) -> void:
+func _physics_process(delta: float) -> void:
 	move ()
 	animate()
 
@@ -17,22 +17,39 @@ func move () -> void:
 	move_and_slide ()
 
 func animate () -> void:
-	if velocity.x != 0 or velocity.y != 0:
+	if velocity.x > 0 or velocity.y > 0:
 		if Input.is_action_just_pressed("ui_left"):
+			texture.flip_h = false
 			current_dir = "left"
 			animation.play("walkSIDE")
 
+		if Input.is_action_just_pressed("ui_right"):
+			texture.flip_h = true
+			current_dir = "right"
+			animation.play("walkSIDE")
+
 		if Input.is_action_pressed("ui_up"):
+			texture.flip_h = false
 			current_dir = "up"
 			animation.play("walkBACK")
 
 		if Input.is_action_pressed("ui_down"):
+			texture.flip_h = false
 			current_dir = "down"
 			animation.play("walkFRONT")
 	else:
 		if current_dir == "left":
 			animation.play("idleSIDE")
+			texture.flip_h = false
+			
 		if current_dir == "up":
 			animation.play("idleBACK")
+			texture.flip_h = false
+		
 		if current_dir == "down":
 			animation.play("idleFRONT")
+			texture.flip_h = false
+		
+		if current_dir == "right":
+			animation.play("idleSIDE")
+			texture.flip_h = true
